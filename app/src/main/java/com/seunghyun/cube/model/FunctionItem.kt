0f9524870f0c0c5d578c4aa74011b6a7c79d2323
SeungHyunMin <contact@seunghyun.in>
2020-08-01 23:34:25 +0900

    Don't show select text on title

--- app/src/main/java/com/seunghyun/cube/model/FunctionItem.kt
+++ app/src/main/java/com/seunghyun/cube/model/FunctionItem.kt
@@ -3,4 +3,6 @@ package com.seunghyun.cube.model
 data class FunctionItem(val skinNumber: Int, val device: String, val function: String) {
     override fun toString() = if (device.isNotBlank() && function.isNotBlank()) "$device $function"
     else "기능을 선택해주세요."
+
+    fun toStringIgnoreEmpty() = "$device $function"
 }
