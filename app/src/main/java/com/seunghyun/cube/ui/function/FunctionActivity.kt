335f02a6d7332397e5a926a51bf5b1170b9f9626
SeungHyunMin <contact@seunghyun.in>
2020-08-01 22:30:25 +0900

    Create FunctionActivity.kt

---
+++ app/src/main/java/com/seunghyun/cube/ui/function/FunctionActivity.kt
@@ -0,0 +1,13 @@
+package com.seunghyun.cube.ui.function
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.seunghyun.cube.R
+
+class FunctionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_function)
+    }
+}
